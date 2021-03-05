c55b7e11d47ab9aea219aac0351b00a060cea576
pengpeng <pengpeng@pin-dao.cn>
2021-03-05 11:53:28 +0800

    bug fixed

--- src/airobotLibrary/__init__.py
+++ src/airobotLibrary/__init__.py
@@ -5,3 +5,8 @@ from DiffLibrary import DiffLibrary as AirDiff
 from RequestsLibrary import RequestsLibrary as AirRequests
 from ArchiveLibrary import ArchiveLibrary as AirArchive
 from FtpLibrary import FtpLibrary as AirFtp
+from airtest.core.settings import Settings as ST
+
+
+ST.REMOTE_URL = None
+ST.BROWSER = 'Chrome'
