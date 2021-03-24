48dabf791b7e7b42e541d18a333efc348b622b6f
无限小数 <forpeng@foxmail.com>
2021-03-25 01:42:12 +0800

    support ios

--- src/airobotLibrary/__init__.py
+++ src/airobotLibrary/__init__.py
@@ -1,6 +1,6 @@
 from .air_selenium import AirSelenium
+from .air_appium import AirAppium
 from DatabaseLibrary import DatabaseLibrary as AirDatabase
-from AppiumLibrary import AppiumLibrary as AirAppium
 from DiffLibrary import DiffLibrary as AirDiff
 from RequestsLibrary import RequestsLibrary as AirRequests
 from ArchiveLibrary import ArchiveLibrary as AirArchive
