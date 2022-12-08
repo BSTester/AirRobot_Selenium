2914194d4552af1746030d05eaab414ab522467e
贝克街的捉虫师 <forpeng@foxmail.com>
2022-12-08 18:25:50 +0800

    bugfix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='AirobotLibrary',  # Required
 
-    version='1.2.3',  # Required
+    version='1.2.4',  # Required
 
     description='Robotframework Library For Airobot Test Framework',  # Optional
 
